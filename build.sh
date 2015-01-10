7f52071321738e9f1522d98fb490dac881294940
Kishore Kumar Vaishnav <kishorevaishnav@gmail.com>
2015-01-10 01:23:21 -0800

    new builds for v0.1

---
+++ build.sh
@@ -0,0 +1,8 @@
+rm *.tar.gz
+rm *.zip
+tar -czvf pravasan_darwin_386.tar.gz pravasan_darwin_386
+tar -czvf pravasan_darwin_amd64.tar.gz pravasan_darwin_amd64
+tar -czvf pravasan_linux_386.tar.gz pravasan_linux_386
+tar -czvf pravasan_linux_amd64.tar.gz pravasan_linux_amd64
+zip pravasan_windows_386.zip pravasan_windows_386.exe
+zip pravasan_windows_amd64.zip pravasan_windows_amd64.exe
