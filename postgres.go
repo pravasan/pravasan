f5f216225323aeffdd54339a0cfbc3e0c20295cd
Kishore Kumar Satyanarayan <kishore@Kishores-MacBook-Air.local>
2015-03-19 23:56:50 -0700

    Added common datatypes for postgres

--- postgres.go
+++ postgres.go
@@ -60,6 +60,12 @@ var PostgreSQLSupportedDataTypes = map[string]string{
 	"DECIMAL":       "DECIMAL",
 	"TIMETZ":        "TIMETZ",
 	"TIMESTAMPTZ":   "TIMESTAMPTZ",
+	"INT":           "INTEGER",      // Alias of INTEGER
+	"DEC":           "DECIMAL",      // Alias of DECIMAL
+	"NUMERIC":       "DECIMAL",      // Alias of DECIMAL
+	"FIXED":         "DECIMAL",      // Alias of DECIMAL
+	"BOOL":          "BOOLEAN",      // Alias of BOOLEAN
+	"STRING":        "VARCHAR(255)", // Alias of VARCHAR
 }
 
 func init() {
